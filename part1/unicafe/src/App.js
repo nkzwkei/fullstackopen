b8d051252bc03d11c6284c0ebdba98cfe72222b0
nkzwkei <nakazawa_kei@proton.me>
2023-06-07 21:53:08 +0300

    Exercise 1.6-1.7

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => {
+    setGood(good+1)
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral+1)
+  }
+
+  const handleBad = () => {
+    setBad(bad+1)
+  }
+
+  const all = good+neutral+bad
+  const average = all === 0 ? 0 : good/all*100
+
+  return (
+    <>
+      <h2>give feedback</h2>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNeutral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average} %</p>
+    </>
+  );
+}
+
+export default App;

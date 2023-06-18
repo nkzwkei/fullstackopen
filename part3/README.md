1420c08eee82088b3898cd63050a577504e7a473
nkzwkei <nakazawa_kei@proton.me>
2023-06-18 22:59:57 +0300

    Exercise 3.12

--- part3/README.md
+++ part3/README.md
@@ -2,3 +2,5 @@ Full app deploy : https://phonebook-test-dwd0.onrender.com/
 
 # minor bug : filter but not returned full 
 # minor bug : delete should return an error
+
+# mongoose.connection.close() must not be placed outside
---
+++ part3/mongo.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+
+if(process.argv.length < 3 || process.argv.length === 4 || process.argv.length > 5) {
+    console.log('Please provide password or enough arguments')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const link = `mongodb+srv://nkzw:${password}@cluster0.wqbfewn.mongodb.net/`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(link)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if(process.argv.length === 3) {
+    Person.find({}).then(persons => {
+        console.log('phonebook:')
+        persons.map(person => {
+            console.log(person.name, person.number)
+        })
+        mongoose.connection.close()
+    })
+}
+
+else if(process.argv.length === 5) {
+    const person = new Person({
+        name : process.argv[3],
+        number : process.argv[4]
+    })
+    person.save().then(result => {
+        console.log(`added ${result.name} ${result.number}`)
+        mongoose.connection.close()
+    })
+}
+
+
+
+

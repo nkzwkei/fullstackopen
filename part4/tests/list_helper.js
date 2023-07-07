45a2e91f836e502fe9530c428e496bf564a02cc7
nkzwkei <nakazawa_kei@proton.me>
2023-07-07 22:29:58 +0300

    Exercise 4.6

--- part4/tests/list_helper.js
+++ part4/tests/list_helper.js
@@ -7,6 +7,16 @@ const totalLikes = (blogs) => {
     return blogs.map(blog => blog.likes).reduce((a, b) => a+b)
 }
 
+const favoriteBlog = (blogs) => {
+    const maxLikes = Math.max(...blogs.map(blog => blog.likes))
+    for(let i=0; i<blogs.length; ++i) {
+        if(blogs[i].likes === maxLikes) {
+            const { title, author, likes } = blogs[i]
+            return { title, author, likes }
+        }
+    }
+}
+
 module.exports = {
-    dummy, totalLikes
+    dummy, totalLikes, favoriteBlog
 }

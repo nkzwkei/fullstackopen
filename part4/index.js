7c351eea79d599e50b8243a11f799e62233e7d9b
nkzwkei <nakazawa_kei@proton.me>
2023-07-07 19:36:35 +0300

    Start doing sth:

--- part4/index.js
+++ part4/index.js
@@ -1,127 +1,43 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
 const express = require('express')
+const app = express()
 const cors = require('cors')
-const morgan = require('morgan')
+const mongoose = require('mongoose')
 require('dotenv').config()
 
-const Person = require('./mongo')
-const app = express()
-
-morgan.token('body', (req, _) => {
-	return JSON.stringify(req.body)
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
 })
 
-app.use(
-	morgan(':method :url :status :res[content-length] - :response-time ms :body')
-)
-app.use(express.json())
-app.use(express.static('build'))
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = process.env.DATABASE_URL
+mongoose.connect(mongoUrl)
+
 app.use(cors())
+app.use(express.json())
 
-const errorHandler = (err, _, res, next) => {
-	console.error(err.message)
-
-	if (err.name === 'CastError') {
-		return res.status(400).send({
-			error: 'malformatted id',
-		})
-	} else if (err.name === 'ValidationError') {
-		console.log('valid error')
-		return res.status(400).json({ error: err.message })
-	}
-
-	next(err)
-}
-
-const unknownEndpoint = (_, response) => {
-	response.status(404).send({ error: 'unknown endpoint' })
-}
-
-app.get('/', (_, res) => {
-	return res.send('<h1>Hi</h1>')
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
 })
 
-app.get('/info', (_, res) => {
-	const today = new Date(Date.now())
-	const length = Person.find({}).length
-	res.send(
-		`<p>Phone book has info for ${length} people<br><p>${today.toUTCString()}</p>`
-	)
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
 })
 
-app.get('/api/persons', (_, res) => {
-	Person.find({}).then((persons) => {
-		return res.json(persons)
-	})
-})
-
-app.get('/api/persons/:id', (req, res, next) => {
-	Person.findById(req.params.id)
-		.then((person) => {
-			return res.json(person)
-		})
-		.catch((error) => next(error))
-})
-
-app.post('/api/persons', (req, res, next) => {
-	const { body } = req
-
-	if (!body.name || !body.number) {
-		return res.status(404).send({
-			error: 'the name or number is missing',
-		})
-	}
-
-	const person = new Person({
-		name: body.name,
-		number: body.number,
-	})
-
-	person
-		.save()
-		.then((savedPerson) => {
-			res.json(savedPerson)
-		})
-		.catch((error) => next(error))
-})
-
-app.put('/api/persons/:id', (req, res, next) => {
-	const { body } = req
-
-	if (!body.name || !body.number) {
-		return res.status(404).send({
-			error: 'the name or number is missing',
-		})
-	}
-
-	const person = {
-		name: body.name,
-		number: body.number,
-	}
-
-	Person.findByIdAndUpdate(req.params.id, person, { new: true })
-		.then((updatedPerson) => {
-			return res.json(updatedPerson)
-		})
-		.catch((error) => next(error))
-})
-
-app.delete('/api/persons/:id', (req, res, next) => {
-	Person.findByIdAndRemove(req.params.id)
-		.then((_) => {
-			return res.status(204).end()
-		})
-		.catch((error) => next(error))
-})
-
-app.use(unknownEndpoint)
-app.use(errorHandler)
-
-const PORT = process.env.PORT || 3001
-
+const PORT = 3003
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
-
-// can't delay today
